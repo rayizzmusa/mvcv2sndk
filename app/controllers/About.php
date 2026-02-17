f6664e878dcc19dce00b0417d5d7e68067f49383
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-02-17 18:30:57 +0700

    first commit bljar mvc, routing url, controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php
+
+class About
+{
+    public function index($nama = 'Syifa')
+    {
+        echo "Saya $nama";
+    }
+    public function page()
+    {
+        echo 'about/page';
+    }
+}
