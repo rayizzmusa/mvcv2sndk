0555fdca00e76fa194da4598e98c73ae122c36d7
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-02-19 22:02:08 +0700

    first commit bljar mvc, routing url, controller, view

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,13 +1,22 @@
 <?php
 
-class About
+class About extends Controller
 {
-    public function index($nama = 'Syifa')
+    public function index($nama = 'Bryan', $pekerjaan = "Guru", $umur = 21)
     {
-        echo "Saya $nama";
+        $data['judul'] = "About Me";
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
     }
     public function page()
     {
-        echo 'about/page';
+        $data['judul'] = "Pages";
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/header');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About me</h1>
+<p>Halo nama saya <?= $data['nama'] ?>, umur <?= $data['umur'] ?>, saya seorang <?= $data['pekerjaan'] ?></p>
