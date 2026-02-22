fd97003dbdbcdcd916c4412a2f6db5776e06aa3e
Rayhan Muhammad <rayhanmm1234@gmail.com>
2026-02-22 12:08:18 +0700

    first commit bljar mvc, routing url, controller, view, assets, model, database wrapper, insert

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -58,4 +58,20 @@ class Mahasiswa_model
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataMhs($data)
+    {
+        $query = "insert into mahasiswa values ('', :nama, :nim, :email, :jurusan)";
+
+        $this->db->query($query);
+
+        $this->db->bind('nama', $data['nama']);
+        $this->db->bind('nim', $data['nim']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('jurusan', $data['jurusan']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,11 @@
-<div class="container mt-5">
+<div class="container mt-3">
     <div class="row">
         <div class="col-6">
             <h3>Daftar Mahasiswa</h3>
+
+            <button type="button" class="btn btn-primary mt-3 mb-3" data-bs-toggle="modal" data-bs-target="#formModal">
+                Tambah Data
+            </button>
 
             <ul class="list-group">
                 <?php foreach ($data['mhs'] as $mahasiswa) : ?>
@@ -12,6 +16,43 @@
                 <?php endforeach ?>
             </ul>
 
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judulmodal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="judulmodal">Tambah Data Mahasiswa</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                    <div class="mb-3">
+                        <input type="text" class="form-control" id="nama" placeholder="Nama" name="nama">
+                    </div>
+                    <div class="mb-3">
+                        <input type="number" class="form-control" id="nim" placeholder="Nim" name="nim">
+                    </div>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <select class="form-select" size="3" aria-label="size 3 select example" name="jurusan">
+                            <option selected>Jurusan</option>
+                            <option value="IT">IT</option>
+                            <option value="B. Arab">B. Arab</option>
+                            <option value="Matematika">Matematika</option>
+                            <option value="IPA">IPA</option>
+                        </select>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
         </div>
     </div>
 </div>
